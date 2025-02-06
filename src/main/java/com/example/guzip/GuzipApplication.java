050d3c0e21fec5af211a1b6e0e525ee4b83f836f
jeongIN <shjin4667@gmail.com>
2025-02-06 21:41:26 +0900

    initial commit

---
+++ src/main/java/com/example/guzip/GuzipApplication.java
@@ -0,0 +1,13 @@
+package com.example.guzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guzip/GuzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
